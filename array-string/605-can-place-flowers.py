9f46b648c62cd457d070b302801bad118e5a74d0
Ross Graves <rgraves89@gmail.com>
2026-02-02 10:04:44 -0700

    Added time & space complexities, also updated formatting.

--- array-string/605-can-place-flowers.py
+++ array-string/605-can-place-flowers.py
@@ -18,10 +18,10 @@ Example 2:
  
 Constraints:
 
-- 1 <= flowerbed.length <= 2 * 104
-- flowerbed[i] is 0 or 1.
-- There are no two adjacent flowers in flowerbed.
-- 0 <= n <= flowerbed.length
+    - 1 <= flowerbed.length <= 2 * 104
+    - flowerbed[i] is 0 or 1.
+    - There are no two adjacent flowers in flowerbed.
+    - 0 <= n <= flowerbed.length
 
 """
 

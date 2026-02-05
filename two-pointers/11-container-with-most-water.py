b6c52d05043a8fe1a3aa866b2f42f9414f8accc5
Ross Graves <rgraves89@gmail.com>
2026-02-04 21:08:26 -0700

    Adding solution for challenge #11.

---
+++ two-pointers/11-container-with-most-water.py
@@ -0,0 +1,53 @@
+"""
+
+11. Container With Most Water
+
+	You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+	Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+	Return the maximum amount of water a container can store.
+
+	Notice that you may not slant the container
+
+Example 1:
+
+	Input: height = [1,8,6,2,5,4,8,3,7]
+	Output: 49
+	Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+Example 2:
+
+	Input: height = [1,1]
+	Output: 1
+
+Constraints:
+
+	- n == height.length
+	- 2 <= n <= 10^5
+	- 0 <= height[i] <= 10^4
+
+"""
+
+# Time complexity: O(N)
+# Space complexity: O(1)
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+    	# Set the left pointer to the beginning of the array and the right to the end
+        left, right = 0, len(height) - 1
+        max_area = 0
+
+        # Loop until the left pointer meets the right pointer
+        while left < right:
+        	# Determine the current area & update max_area
+            width = right - left
+            current_height = min(height[left], height[right])
+            max_area = max(max_area, current_height * width)
+
+            # If left height is smaller than the right, advance the left pointer. Otherwise, move the right pointer back.
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
